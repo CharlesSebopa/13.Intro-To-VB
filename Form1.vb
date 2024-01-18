895d3203db17146ef7a7f956c2bb5a4413d91918
Lenovo-User <Lenovo-User@DESKTOP-7L4SHKG>
2024-01-18 16:33:13 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim firstnumber As Double
+        Dim secondnumber As Double
+
+        firstnumber = Val(TextBox1.Text)
+        secondnumber = Val(TextBox2.Text)
+
+        If firstnumber > secondnumber Then
+            TextBox3.Text = firstnumber
+        Else
+            TextBox3.Text = secondnumber
+        End If
+    End Sub
+End Class
